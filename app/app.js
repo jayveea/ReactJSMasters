b3ee00d176f17bade9d07ce043a2fb7122c212fe
JayveeA <jayvee.lyn@gmail.com>
2017-02-16 18:22:06 +0800

    cleaned up references

--- app/app.js
+++ app/app.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
-import ReactBootstrap from 'react-bootstrap';
 import Container from './components/container.js';
 import TaskManager from './taskManager.js';
 import About from './components/about.js';
-import Layout from './components/layout.js';
 
 class KanbanApplication extends React.PureComponent{
   render(){
